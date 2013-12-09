bc4d33108bb68ce02d5415673955e12cb9d2a433
Thomas <tgrimault@tamaggo.com>
2013-12-08 20:58:49 -0500

    main: add a 5s delay in the main loop to lower CPU usage.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <chrono>
 #include "motionapp.h"
+
+using namespace std;
 
 int main() 
 {
+	chrono::milliseconds TicTac( 5000 );	// 5s
 	MotionApp * app;
 	
 	app = new MotionApp();
 	app->start();
 	
-	while (1);
+	while (1) {
+		// nothing else to but sleeping...
+		cout << "Surveillance in progress ...\n";
+		this_thread::sleep_for( TicTac );
+	}
 	
 	return 0;
 }
