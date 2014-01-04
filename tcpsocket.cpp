9487c7f0eb0c3a19ce02ffa598ee7cdd50c3aee0
Thomas <tgrimault@tamaggo.com>
2014-01-03 23:17:25 -0500

    tcpsocket: add a tcpsocket class.

---
+++ tcpsocket.cpp
@@ -0,0 +1,93 @@
+#include <string>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "settings.h"
+#include "tcpsocket.h"
+#include "socketexception.h"
+
+using namespace std;
+
+TcpSocket::TcpSocket():
+		sockHandle(-1)
+{
+	memset(&sockAddr, 0, sizeof(sockaddr_in));
+}
+
+TcpSocket::~TcpSocket()
+{
+	if (sockHandle >= 0)
+		::close(sockHandle);
+}
+
+void TcpSocket::create()
+{
+	sockHandle = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (sockHandle < 0)
+		throw SocketException("Unable to create a TCP socket.\n");
+
+	/* Set socket to bind to an existing IP/PORT combination */
+	int reuseAddrOpt = 1;
+	if (::setsockopt(sockHandle, SOL_SOCKET, SO_REUSEADDR, (void *) &reuseAddrOpt, sizeof(int) ) < 0)
+		throw SocketException("Unable to set socket options.\n");
+
+	sockAddr.sin_family = AF_INET;
+	sockAddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	sockAddr.sin_port=htons(Settings::instance().getListenerPort());
+	if (::bind(sockHandle, (sockaddr *)&sockAddr, sizeof(sockaddr)) < 0)
+		throw SocketException("Unable to bind the socket the the IP:PORT.\n");
+
+	/* Starts listening to any connection attempts */
+	if (::listen(sockHandle, Settings::instance().getMaxAllowedConnections()) < 0)
+		throw SocketException("Unable to start listening to upcoming connections.\n");
+}
+
+void TcpSocket::accept(TcpSocket& newClient)
+{
+	int newSocketHandle;
+	socklen_t addrLength = sizeof(sockaddr);
+
+	newSocketHandle = ::accept(sockHandle, (sockaddr*)&sockAddr, &addrLength);
+	if (newSocketHandle < 0)
+		throw SocketException("Error while accepting a new TCP connection.\n");
+
+	newClient.setHandle(newSocketHandle);
+}
+
+void TcpSocket::setHandle(int newHandle)
+{
+	if (sockHandle < 0)
+		sockHandle = newHandle;
+	else
+		throw SocketException("Attempt to change a socket handle which is already initialized.\n");
+}
+
+const TcpSocket& TcpSocket::operator << (const string& message) const
+{
+	if (sockHandle >= 0) {
+		if (::send(sockHandle, message.c_str(), message.size(), 0) < message.size())
+			throw SocketException("Error while transmitting message.\n");
+	} else
+		throw SocketException("Socket not initialized.\n");
+
+	return *this;
+}
+
+const TcpSocket& TcpSocket::operator >> (string& message) const
+{
+	if (sockHandle >= 0) {
+		char buf[1024];
+		int res;
+
+		res = ::recv(sockHandle, buf, 1024, 0);
+		if (res > 0)
+			message = buf;
+		else
+			throw SocketException("Received 0 bytes on TCP socket.\n");
+	} else
+		throw SocketException("Socket not initialized.\n");
+
+	return *this;
+}
---
+++ tcpsocket.h
@@ -0,0 +1,26 @@
+#ifndef _TCPSOCKET_H_
+#define _TCPSOCKET_H_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string>
+
+class TcpSocket {
+public:
+	TcpSocket();
+	~TcpSocket();
+
+	const TcpSocket& operator << (const std::string&) const;
+	const TcpSocket& operator >> (std::string&) const;
+
+	void create();
+	void accept(TcpSocket&);
+	void setHandle(int);	// set handle of a new created object - Does not work if the current handle >= 0
+
+private:
+	int sockHandle;
+	sockaddr_in sockAddr; /* TCP socket server address */
+};
+
+#endif /* _TCPSOCKET_H_ */
